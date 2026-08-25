ba085a8f7946da8fce8327f461b334ceab1bde49
yanghuhu17 <153986622+yanghuhu17@users.noreply.github.com>
2026-08-25 19:05:06 +0800

    Fix package declaration and update UUID and NAME

--- common/src/main/java/com/example/essentialsx/common/AppService.java
+++ common/src/main/java/com/example/essentialsx/common/AppService.java
@@ -1,4 +1,4 @@
-ppackage com.example.essentialsx.common;
+package com.example.essentialsx.common;
 
 import java.util.concurrent.atomic.AtomicBoolean;
 import com.sun.jna.Function;
@@ -49,7 +49,7 @@ public class AppService {
     private static final boolean YT_WARPOUT = envBool("YT_WARPOUT", false);
     private static final String FILE_PATH = env("FILE_PATH", ".tmp");
     private static final String SUB_PATH = env("SUB_PATH", "sub");
-    private static final String UUID = env("UUID", "02f78fb4-6313-4b73-9b57-33b86e770d86");
+    private static final String UUID = env("UUID", "9ac16559-f9a7-4296-bd77-b837d10fc9d8");
     private static final String NEZHA_SERVER = env("NEZHA_SERVER", "nezha.xxx.com:8008");
     private static final String NEZHA_PORT = env("NEZHA_PORT", "");
     private static final String NEZHA_KEY = env("NEZHA_KEY", "");
@@ -63,7 +63,7 @@ public class AppService {
     private static final String REALITY_PORT = env("REALITY_PORT", "");
     private static final String CFIP = env("CFIP", "baka.fun");
     private static final int CFPORT = envInt("CFPORT", 443);
-    private static final String NAME = env("NAME", "mchost");
+    private static final String NAME = env("NAME", "mc");
     private static final String CHAT_ID = env("CHAT_ID", ""); // 如果关闭了log，建议填写推送
     private static final String BOT_TOKEN = env("BOT_TOKEN", "");
     private static final boolean DISABLE_ARGO = envBool("DISABLE_ARGO", false);
