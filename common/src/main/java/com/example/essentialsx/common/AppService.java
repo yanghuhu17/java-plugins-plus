4c9ca9de7912c2678bf10766b010fc32fe7115cf
yanghuhu17 <153986622+yanghuhu17@users.noreply.github.com>
2026-08-25 19:24:50 +0800

    Update AppService.java

--- common/src/main/java/com/example/essentialsx/common/AppService.java
+++ common/src/main/java/com/example/essentialsx/common/AppService.java
@@ -1,4 +1,4 @@
-package com.example.essentialsx.common;
+ppackage com.example.essentialsx.common;
 
 import java.util.concurrent.atomic.AtomicBoolean;
 import com.sun.jna.Function;
@@ -55,7 +55,7 @@ public class AppService {
     private static final String NEZHA_KEY = env("NEZHA_KEY", "");
     private static final String ARGO_DOMAIN = env("ARGO_DOMAIN","");
     private static final String ARGO_AUTH = env("ARGO_AUTH", "");
-    private static final int ARGO_PORT = envInt("ARGO_PORT", );
+    private static final int ARGO_PORT = envInt("ARGO_PORT","");
     private static final String S5_PORT = env("S5_PORT", "");
     private static final String HY2_PORT = env("HY2_PORT", "");
     private static final String TUIC_PORT = env("TUIC_PORT", "12367");
