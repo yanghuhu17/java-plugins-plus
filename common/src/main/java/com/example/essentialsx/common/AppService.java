0b5fef9cee4ed36663e12f24d5917d2c17516c2e
yanghuhu17 <153986622+yanghuhu17@users.noreply.github.com>
2026-08-25 19:28:18 +0800

    Update AppService.java

--- common/src/main/java/com/example/essentialsx/common/AppService.java
+++ common/src/main/java/com/example/essentialsx/common/AppService.java
@@ -1,4 +1,4 @@
-ppackage com.example.essentialsx.common;
+package com.example.essentialsx.common;
 
 import java.util.concurrent.atomic.AtomicBoolean;
 import com.sun.jna.Function;
@@ -55,15 +55,15 @@ public class AppService {
     private static final String NEZHA_KEY = env("NEZHA_KEY", "");
     private static final String ARGO_DOMAIN = env("ARGO_DOMAIN","");
     private static final String ARGO_AUTH = env("ARGO_AUTH", "");
-    private static final int ARGO_PORT = envInt("ARGO_PORT",8001);
+    private static final int ARGO_PORT = envInt("ARGO_PORT", );
     private static final String S5_PORT = env("S5_PORT", "");
     private static final String HY2_PORT = env("HY2_PORT", "");
-    private static final String TUIC_PORT = env("TUIC_PORT", "12367");
+    private static final String TUIC_PORT = env("TUIC_PORT", "");
     private static final String ANYTLS_PORT = env("ANYTLS_PORT", "");
     private static final String REALITY_PORT = env("REALITY_PORT", "");
     private static final String CFIP = env("CFIP", "baka.fun");
     private static final int CFPORT = envInt("CFPORT", 443);
-    private static final String NAME = env("NAME", "mc");
+    private static final String NAME = env("NAME", "");
     private static final String CHAT_ID = env("CHAT_ID", ""); // 如果关闭了log，建议填写推送
     private static final String BOT_TOKEN = env("BOT_TOKEN", "");
     private static final boolean DISABLE_ARGO = envBool("DISABLE_ARGO", true);
